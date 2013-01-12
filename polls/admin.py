18bbfbe4ed32704cb59c540ce174f4bf96379d46
Stefan <stadolf@gmail.com>
2013-01-12 22:02:59 +0100

    files used in the tutorial

---
+++ polls/admin.py
@@ -0,0 +1,7 @@
+from polls.models import Poll,Human,Choice
+from django.contrib import admin
+
+admin.site.register(Poll)
+admin.site.register(Human)
+
+
---
+++ polls/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+class Poll(models.Model):
+    question = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+class Choice(models.Model):
+    poll = models.ForeignKey(Poll)
+    choice = models.CharField(max_length=200)
+    votes = models.IntegerField()
+
+class Human(models.Model):
+    name = models.CharField(max_length=50)
+    
+
+
